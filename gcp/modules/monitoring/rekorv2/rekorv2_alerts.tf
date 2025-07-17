83a30957298141bc241555051dd346244b2071d7
Colleen Murphy <colleenmurphy@google.com>
2025-07-17 11:59:43 -0700

    Add monitoring for Rekor v2
    Copy the relevant alerts from Rekor v1 to a new Rekor v2 submodule in
    monitoring.
    
    Like the main tiles_tlog module, this module may be re-used for
    individual shards, so it is not included in the monitoring module's
    sigstore.tf composition and instead must be individually called at the
    same level the tiles_tlog module is called.
    
    The module is named rekorv2 instead of tiles_tlog because some of the
    metrics are specific to the rekor-tiles service and would not be
    relevant to a generic transparency log. This is also why it must be
    called on its own and is not included as part of the tiles_tlog module.
    
    There is no uptime check configured for the service itself because Rekor
    v2 has no native publicly exposed GET endpoint and it would not be an
    effective use of the log to POST entries to it every 30 seconds. The
    prober will check that the service is up and working, and the Kubernetes
    alerts should notify of the service behaving badly.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ gcp/modules/monitoring/rekorv2/rekorv2_alerts.tf
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2025 The Sigstore Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+### K8s Alerts
+
+resource "google_monitoring_alert_policy" "rekorv2_k8s_pod_restart_failing_container" {
+  # adding a dependency on the associated metric means that Terraform will 
+  # always try to apply changes to the metric before this alert
+  depends_on = [google_logging_metric.rekorv2_k8s_pod_restart_failing_container]
+
+  # In the absence of data, incident will auto-close in 7 days
+  alert_strategy {
+    auto_close = "604800s"
+  }
+
+  combiner = "OR"
+
+  conditions {
+    condition_threshold {
+      aggregations {
+        alignment_period   = "600s"
+        per_series_aligner = "ALIGN_COUNT"
+      }
+
+      comparison              = "COMPARISON_GT"
+      duration                = "600s"
+      evaluation_missing_data = "EVALUATION_MISSING_DATA_NO_OP"
+      filter                  = "metric.type=\"logging.googleapis.com/user/rekorv2/k8s_pod/restarting-failed-container\" resource.type=\"k8s_pod\""
+      threshold_value         = "1"
+
+      trigger {
+        count   = "1"
+        percent = "0"
+      }
+    }
+
+    display_name = "K8s Restart Failing Container for more than ten minutes"
+  }
+
+  display_name = "Rekor V2 K8s Restart Failing Container"
+
+  documentation {
+    content   = "K8s is restarting a failing container for longer than the accepted time limit, please see playbook for help.\n"
+    mime_type = "text/markdown"
+  }
+
+  enabled               = "true"
+  notification_channels = local.notification_channels
+  project               = var.project_id
+}
+
+resource "google_monitoring_alert_policy" "rekorv2_k8s_pod_unschedulable" {
+  # adding a dependency on the associated metric means that Terraform will 
+  # always try to apply changes to the metric before this alert
+  depends_on = [google_logging_metric.k8s_pod_unschedulable]
+
+  # In the absence of data, incident will auto-close in 7 days
+  alert_strategy {
+    auto_close = "604800s"
+  }
+
+  combiner = "OR"
+
+  conditions {
+    condition_threshold {
+      aggregations {
+        alignment_period   = "600s"
+        per_series_aligner = "ALIGN_COUNT"
+      }
+
+      comparison      = "COMPARISON_GT"
+      duration        = "600s"
+      filter          = "metric.type=\"logging.googleapis.com/user/rekorv2/k8s_pod/unschedulable\" resource.type=\"k8s_pod\""
+      threshold_value = "1"
+
+      trigger {
+        count   = "1"
+        percent = "0"
+      }
+    }
+
+    display_name = "K8s was unable to schedule a pod for more than ten minutes"
+  }
+
+  display_name = "Rekor V2 K8s Unscheduable"
+
+  documentation {
+    content   = "K8s is restarting a failing container for longer than the accepted time limit, please see playbook for help."
+    mime_type = "text/markdown"
+  }
+
+  enabled               = "true"
+  notification_channels = local.notification_channels
+  project               = var.project_id
+}
