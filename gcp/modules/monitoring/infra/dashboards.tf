17fc7ddb76d10ddb440b33fc0a11308926a7dfaf
Colleen Murphy <colleenmurphy@google.com>
2025-08-13 16:28:46 -0700

    Add alerts and dashboards for Spanner
    Configure alerts for CPU and disk usage as recommended in the
    documentation[1]. Add dashboards for CPU usage - there is already a
    default dashboard for bytes used.
    
    [1] https://cloud.google.com/spanner/docs/monitoring-cloud#create-alert
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ gcp/modules/monitoring/infra/dashboards.tf
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2025 The Sigstore Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "google_monitoring_dashboard" "spanner_cpu_dashboard" {
+  dashboard_json = <<EOF
+{
+  "displayName": "Spanner CPU Alerts",
+  "dashboardFilters": [],
+  "labels": {},
+  "mosaicLayout": {
+    "columns": 48,
+    "tiles": [
+      {
+        "height": 16,
+        "width": 24,
+        "widget": {
+          "title": "",
+          "alertChart": {
+            "name": "${google_monitoring_alert_policy.spanner_high_priority_cpu_utilization_warning.id}"
+          },
+          "id": ""
+        }
+      },
+      {
+        "xPos": 24,
+        "height": 16,
+        "width": 24,
+        "widget": {
+          "alertChart": {
+            "name": "${google_monitoring_alert_policy.spanner_smoothed_cpu_utilization_warning.id}"
+          }
+        }
+      }
+    ]
+  }
+}
+EOF
+
+  project = var.project_id
+
+  depends_on = [
+    google_monitoring_alert_policy.spanner_high_priority_cpu_utilization_warning,
+    google_monitoring_alert_policy.spanner_smoothed_cpu_utilization_warning
+  ]
+}
