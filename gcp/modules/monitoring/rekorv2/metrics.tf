646c1c25fdfe7786d63f764b6d08cd27d31cac22
Appu Goundan <appu@google.com>
2025-09-15 14:04:30 -0400

    Add metrics template for rekorv2 lb
    Signed-off-by: Appu Goundan <appu@google.com>

--- gcp/modules/monitoring/rekorv2/metrics.tf
+++ gcp/modules/monitoring/rekorv2/metrics.tf
@@ -41,3 +41,49 @@ resource "google_logging_metric" "k8s_pod_unschedulable" {
   name    = "rekorv2-${var.shard_name}/k8s_pod/unschedulable"
   project = var.project_id
 }
+
+resource "google_logging_metric" "rekor_v2_lb_requests" {
+  project     = var.project_id
+  name        = "rekorv2-${var.shard_name}/lb_requests"
+  description = "Counts the requests at the load balancer"
+  filter      = "httpRequest.requestUrl=~\"^https://${var.shard_name}\\.rekor\\.sigstage\\.dev\" AND resource.type=\"http_load_balancer\""
+
+  metric_descriptor {
+    metric_kind = "DELTA"
+    unit        = "1"
+    value_type  = "INT64"
+    labels {
+      key        = "cache_hit"
+      value_type = "BOOL"
+    }
+    labels {
+      key        = "cache_lookup"
+      value_type = "BOOL"
+    }
+    labels {
+      key        = "http_method"
+      value_type = "STRING"
+    }
+    labels {
+      key        = "http_status"
+      value_type = "INT64"
+    }
+    labels {
+      key        = "user_agent_client"
+      value_type = "STRING"
+    }
+    labels {
+      key        = "user_agent_version"
+      value_type = "STRING"
+    }
+
+    labelExtractors = {
+      "cache_hit"          = EXTRACT(httpRequest.cacheHit)
+      "cache_lookup"       = EXTRACT(httpRequest.cacheLookup)
+      "http_method"        = EXTRACT(httpRequest.requestMethod)
+      "http_status"        = EXTRACT(httpRequest.status)
+      "user_agent_client"  = REGEXP_EXTRACT(httpRequest.userAgent, "^([^/]+)")
+      "user_agent_version" = REGEXP_EXTRACT(httpRequest.userAgent, "[^/]+/v?([^\\s]*)")
+    }
+  }
+}
